b90995f965b1c7a5019bc9a840d39f53e385ee5a
x148128 <rana.das@hrblock.com>
2016-11-29 18:27:00 +0000

    hider globals from ang-app.js

--- public/javascripts/ang-app.js
+++ public/javascripts/ang-app.js
@@ -1,34 +1,37 @@
-'use strict';
+(function () {
+    'use strict';
 //var angularApp = angular.module('angularApp', ['ui.router']);
-var angularApp = angular.module('angularApp', ['ngRoute', 'contactsModule']);
+    var angularApp = angular.module('angularApp', ['ngRoute', 'contactsModule']);
 
 
-function config($routeProvider, $locationProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: './partials/home.html',
-            controller: homeController,
-            controllerAs: 'vm'
-        })
-        .when('/register', {
-            templateUrl: './partials/register.html',
-            controller: registerController,
-            controllerAs: 'vm'
-        })
-        .when('/login', {
-            templateUrl: './partials/login.html',
-            controller: loginController,
-            controllerAs: 'vm'
-        })
-        .when('/profile', {
-            templateUrl: 'partials/profile.html',
-            controller: profileController,
-            controllerAs: 'vm'
-        })
-        .otherwise({redirectTo: '/'});
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: './partials/home.html',
+                controller: homeController,
+                controllerAs: 'vm'
+            })
+            .when('/register', {
+                templateUrl: './partials/register.html',
+                controller: registerController,
+                controllerAs: 'vm'
+            })
+            .when('/login', {
+                templateUrl: './partials/login.html',
+                controller: loginController,
+                controllerAs: 'vm'
+            })
+            .when('/profile', {
+                templateUrl: 'partials/profile.html',
+                controller: profileController,
+                controllerAs: 'vm'
+            })
+            .otherwise({redirectTo: '/'});
 
-    // use the HTML5 History API
-    $locationProvider.html5Mode(true);
-}
+        // use the HTML5 History API
+        $locationProvider.html5Mode(true);
+    }
 
-angularApp.config(['$routeProvider', '$locationProvider', config]);
+    angularApp.config(['$routeProvider', '$locationProvider', config]);
+
+})();
