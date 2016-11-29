57d88d747d1e3d6566564debfceb04292362efc9
x148128 <rana.das@hrblock.com>
2016-11-29 19:15:06 +0000

    wiring up service calls to controllers

--- public/javascripts/ang-controllers.js
+++ public/javascripts/ang-controllers.js
@@ -3,7 +3,7 @@ var homeController = function HomePageController() {
     console.log('In Home Page Controller');
 };
 
-var registerController = function RegisterPageController(contactsService) {
+var registerController = function RegisterPageController($location, contactsService) {
     console.log('In Register Page Controller');
     var self = this;
 
@@ -14,16 +14,23 @@ var registerController = function RegisterPageController(contactsService) {
     };
 
     self.onSubmit = function () {
-        console.log('Registering  with '+JSON.stringify(self.credentials));
-        contactsService.register(self.credentials);
+        console.log('Registering  with ' + JSON.stringify(self.credentials));
+        contactsService.register(self.credentials)
+            .error(function (err) {
+                alert(err);
+            })
+            .then(function () {
+                $location.path('profile');
+            });
     }
 };
+registerController.$inject = ['$location', 'contactsService'];
 
 var profileController = function ProfilePageController() {
     console.log('In Profile Page Controller');
 };
 
-var loginController = function LoginPageController(contactsService) {
+var loginController = function LoginPageController($location, contactsService) {
     console.log('In Login Page Controller');
     var self = this;
 
@@ -34,5 +41,13 @@ var loginController = function LoginPageController(contactsService) {
 
     self.onSubmit = function () {
         console.log('In Login Page, submit..');
+        contactsService.login(self.credentials)
+            .error(function(err){
+                alert(err);
+            })
+            .then(function(){
+                $location.path('profile');
+            });
     }
 };
+loginController.$inject = ['$location', 'contactsService'];
--- public/javascripts/ang-services.js
+++ public/javascripts/ang-services.js
@@ -1,29 +1,85 @@
 'use strict';
 
-
 var contactsModuleVariable = angular.module('contactsModule', []);
 
 contactsModuleVariable.factory('contactsService', ContactService);
 
+ContactService.$inject = ['$http', '$window'];
 // Function used in service
-function ContactService($http, $q) {
+function ContactService($http, $window) {
     var service = {};
 
-    service.register = regist;
+    service.saveToken = saveToken;
+    service.getToken = getToken;
+    service.isLoggedIn = isLoggedIn;
+    service.currentUser = currentUser;
+    service.register = register;
+    service.login = login;
+    service.logout = logout;
 
-    // important returned object.
-    return service;
-    function regist(user) {
-        console.log('Registering a user !');
-        return $http.post('/api/register', user)
-            .success(function (data, status, headers, config) {
-                console.log(data.data);
-            })
-            .error(function (data, status, header, config) {
-                console.log(data.data);
-            });
+    function saveToken(token) {
+        $window.localStorage['mean-token'] = token;
+    };
 
-    }
+    function getToken() {
+        return $window.localStorage['mean-token'];
+    };
+
+    function isLoggedIn() {
+        var token = getToken();
+        var payload;
+
+        if (token) {
+            payload = token.split('.')[1];
+            payload = $window.atob(payload);
+            payload = JSON.parse(payload);
+
+            return payload.exp > Date.now() / 1000;
+        } else {
+            return false;
+        }
+    };
+
+    function currentUser() {
+        if (isLoggedIn()) {
+            var token = getToken();
+            var payload = token.split('.')[1];
+            payload = $window.atob(payload);
+            payload = JSON.parse(payload);
+            return {
+                email: payload.email,
+                name: payload.name
+            };
+        }
+    };
+
+    function register(user) {
+        return $http.post('/api/register', user).success(function (data) {
+            saveToken(data.token);
+        });
+    };
+
+    function login(user) {
+        return $http.post('/api/login', user).success(function (data) {
+            saveToken(data.token);
+        });
+    };
+
+    function logout() {
+        $window.localStorage.removeItem('mean-token');
+    };
+
+    //function regist(user) {
+    //    console.log('Registering a user !');
+    //    return $http.post('/api/register', user)
+    //        .success(function (data, status, headers, config) {
+    //            console.log(data.data);
+    //        })
+    //        .error(function (data, status, header, config) {
+    //            console.log(data.data);
+    //        });
+    //
+    //}
 
     // private functions
     function handleSuccess(res) {
@@ -33,4 +89,7 @@ function ContactService($http, $q) {
     function handleError(res) {
         return $q.reject(res.data);
     }
+
+    // important returned object.
+    return service;
 }
