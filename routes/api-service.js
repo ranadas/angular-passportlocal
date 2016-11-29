960880ce80bc5b068254de69d8211adba0861627
x148128 <rana.das@hrblock.com>
2016-11-29 11:41:29 +0000

    fixed api calls, DO NOT EVER have SPACES in url mapping.

--- routes/api-service.js
+++ routes/api-service.js
@@ -4,21 +4,18 @@ const router    = express.Router();
 const chalk = require('chalk');
 const log = console.log;
 
-router.post('/register ', function (req, res) {
+router.post('/register', function (req, res) {
     log(chalk.green("POSTing to register:") + chalk.red(req.body));
-    res.status(200);
-    res.json({
-        "message" : "User registered: " + req.body
-    });
+    res.status(200).json({"message" : "User registered: " + req.body});
 });
 
-router.post('/login ', function (req, res) {
+router.post('/login', function (req, res) {
     log(chalk.green("POSTing to login:") + chalk.red(req.body));
-    res.json("WIP");
+    res.status(200).json("WIP");
 });
-router.get('/profile/USERID ', function (req, res) {
+router.get('/profile/USERID', function (req, res) {
     log(chalk.green("GETing to get profile:") + chalk.red(req.body));
-    res.json("WIP");
+    res.status(200).json("WIP");
 });
 
 require('./route-docu')(router.stack, 'express');
